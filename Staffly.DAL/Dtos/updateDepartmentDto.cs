025474edded2268b44ad028f593e2f16841331d1
Your Name <yousefibrahim9@outlook.com>
2025-04-02 20:17:20 +0200

    Add department details and update functionality.

---
+++ Staffly.DAL/Dtos/updateDepartmentDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staffly.DAL.Dtos
+{
+    public class UpdateDepartmentDto
+    {
+
+        [Required(ErrorMessage = "Code Is Required!")]
+        public string Code { get; set; }
+        [Required(ErrorMessage = "Name Is Required!")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Date Is Required!")]
+        public DateTime CreateAt { get; set; }
+    }
+}
--- Staffly.PL/Controllers/DepartmentController.cs
+++ Staffly.PL/Controllers/DepartmentController.cs
@@ -45,5 +45,68 @@ namespace Staffly.PL.Controllers
 
             return View(departmentDto);
         }
+
+        [HttpGet]
+        public IActionResult Details(int? id)
+        {
+            if(id is null)
+            {
+                return BadRequest("Invalid Id!");
+            }
+
+            var department = _departmentRepository.GetById(id.Value);
+
+            if(department is null)
+            {
+                return NotFound(new {statusCode = 404, message = $"Department With Id:{id.Value} Is Not Found!"});
+            }
+
+            return View(department);
+        }
+
+        [HttpGet]
+        public IActionResult Update(int? id)
+        {
+            if (id is null)
+            {
+                return BadRequest("Invalid Id!");
+            }
+
+            var department = _departmentRepository.GetById(id.Value);
+
+            if (department is null)
+            {
+                return NotFound(new { statusCode = 404, message = $"Department With Id:{id.Value} Is Not Found!" });
+            }
+
+            var departmentDto = new UpdateDepartmentDto
+            {
+                Code = department.Code,
+                Name = department.Name,
+                CreateAt = department.CreateAt
+            };
+
+            return View(departmentDto);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update([FromRoute]int id,UpdateDepartmentDto departmentDto)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var department = new Department
+                {
+                    Id = id,
+                    Code = departmentDto.Code,
+                    Name = departmentDto.Name,
+                    CreateAt = departmentDto.CreateAt
+                };
+
+                _departmentRepository.Update(department);
+                return RedirectToAction("Index");
+            }
+            return View(departmentDto);
+        }
     }
 }
